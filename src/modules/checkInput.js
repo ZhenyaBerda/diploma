2ff0da96ce026d70bf084f03a872efd4bafcc359
unknown <bev030997@gmail.com>
2020-03-12 23:36:35 +0300

    deleted check phone method

--- src/modules/checkInput.js
+++ src/modules/checkInput.js
@@ -9,30 +9,6 @@ const checkInput = () => {
 
         const mask = '+7 (ddd) ddd-dd-dd';
         if (target.closest('#input-phone')) {
-            if (/[0-9]/.test(event.key)) {
-
-                let curValue = target.value,
-                    curLenght = curValue.length;
-
-                if (/[0-9]/.test(event.key)) {
-
-                    if (mask[curLenght] === 'd') {
-
-                        target.value = curValue + event.key;
-                    } else {
-                        for (let i = curLenght; i < mask.length; i++) {
-                            if (mask[i] === 'd') {
-                                target.value = curValue + event.key;
-                                return;
-                            }
-                            curValue += mask[i];
-                        }
-                    }
-
-                }
-            } else {
-                target.value = target.value.replace();
-            }
 
         }
 
