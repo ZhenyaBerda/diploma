27f17729c716ba4b87e62a92be1a616ae840c071
unknown <bev030997@gmail.com>
2020-03-12 20:32:27 +0300

    slider-carousel made (without adaptive)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import toggleMenu from './modules/toggleMenu';
 import togglePhones from './modules/togglePhones';
 import togglePopupRepair from './modules/togglePopupRepair';
 import checkInput from './modules/checkInput';
+import SliderCarousel from './modules/SliderCarousel';
 
 // меню бургер
 toggleMenu();
@@ -13,3 +14,30 @@ togglePhones();
 togglePopupRepair();
 //проверка вводимых значений (еще не сделано :с)
 checkInput();
+
+// слайдер-карусель
+
+
+const carousel = new SliderCarousel({
+    main: '.partner-wrapper',
+    wrap: '.partners-slider',
+    next: '#partners-arrow_right',
+    prev: '#partners-arrow_left',
+    slidesToShow: 3,
+    infinity: true,
+    responsive: [{
+        breakpoint: 1024,
+        slideToShow: 3
+    },
+    {
+        breakpoint: 768,
+        slideToShow: 2
+    },
+    {
+        breakpoint: 576,
+        slideToShow: 1
+    }
+    ],
+});
+
+carousel.init();
--- src/modules/checkInput.js
+++ src/modules/checkInput.js
@@ -2,15 +2,41 @@
 
 const checkInput = () => {
     // id="input-phone"
-  //  const regExp = /^\+?[78]([-()]*\d){10}&/;
 
-    document.body.addEventListener('input', (event) => {
+    document.body.addEventListener('keydown', (event) => {
+
         const target = event.target;
 
+        const mask = '+7 (ddd) ddd-dd-dd';
         if (target.closest('#input-phone')) {
-            target.value = target.value.replace(/^[^+]{1}[^\d]/g, '');
+            if (/[0-9]/.test(event.key)) {
+
+                let curValue = target.value,
+                    curLenght = curValue.length;
+
+                if (/[0-9]/.test(event.key)) {
+
+                    if (mask[curLenght] === 'd') {
+
+                        target.value = curValue + event.key;
+                    } else {
+                        for (let i = curLenght; i < mask.length; i++) {
+                            if (mask[i] === 'd') {
+                                target.value = curValue + event.key;
+                                return;
+                            }
+                            curValue += mask[i];
+                        }
+                    }
+
+                }
+            } else {
+                target.value = target.value.replace();
+            }
+
         }
-    })
+
+    });
 
 };
 
