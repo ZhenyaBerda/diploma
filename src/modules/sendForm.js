6f24e626a9a3b84ae8b6e44abb8b1f68fb65c042
unknown <bev030997@gmail.com>
2020-03-13 14:15:37 +0300

    send forms done

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -31,7 +31,101 @@ const sendForm = () => {
         }
     });
 
-    
+    const postData = (body) => {
+
+        return fetch('./server.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        });
+    };
+
+    const checkInputs = (form) => {
+        const allInputs = form.querySelectorAll('input');
+
+        let check = true;
+
+        allInputs.forEach(input => {
+            console.log(input);
+            if (input.matches('.checkbox__input') && !input.checked) {
+                check = false;
+            }
+
+            if (!input.matches('.checkbox__input') && input.value.length === 0) {
+                check = false;
+            }
+        });
+        return check;
+    };
+
+    //открываем popup-thank
+    const showPopupThank = () => {
+        const popupThank = document.querySelector('.popup-thank');
+        popupThank.style.visibility = 'visible';
+
+        document.body.addEventListener('click', (event) => {
+            const target = event.target;
+
+            //закрываем окно
+            if (target.closest('.close') || !target.closest('.popup-thank-bg')) {
+                popupThank.style.visibility = 'hidden';
+            }
+
+        });
+    };
+
+    const statusMessage = document.createElement('div');
+
+    statusMessage.style.cssText = `color: red; text-align: center;`;
+
+    // отлавливаем нажатие на нужные кнопки
+    document.body.addEventListener('submit', (event) => {
+        statusMessage.textContent = '';
+        const target = event.target;
+
+
+        if (target.closest('form')) {
+            event.preventDefault();
+
+            const form = target.closest('form');
+
+            // для каждого вида формы вставляем текст в свое место, чтобы красивее было
+            if (form.matches('.feedback__form')) {
+                form.insertBefore(statusMessage, form.querySelector('button'));
+            } else {
+                form.appendChild(statusMessage);
+            }
+
+            if (checkInputs(form)) {
+                const formData = new FormData(form);
+
+                let body = {};
+
+                formData.forEach((val, key) => {
+                    body[key] = val;
+                });
+
+
+                // отправляем данные
+                postData(body)
+                    .then((response) => {
+                        if (response.status !== 200) {
+                            throw new Error('status network error');
+                        }
+                        showPopupThank();
+                        form.reset();
+                    })
+                    .catch(statusMessage.textContent = 'Ошибка соединения');
+
+            } else {
+                statusMessage.textContent = 'Введите свои данные и согласитесь с политикой конфиденциальности';
+                return;
+            }
+        }
+
+    });
 
 };
 
