db6249901cbf4606c0b918b0bd457d381b344363
unknown <bev030997@gmail.com>
2020-03-15 01:40:07 +0300

    add phone  mask

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,36 +1,13 @@
 'use strict';
 
+import maskPhone from './maskPhone';
+
 const sendForm = () => {
 
-    // маска для ввеода телефона (не доделанная)
-    document.body.addEventListener('input', (event) => {
-        const target = event.target;
-
-        if (target.closest('.input-phone')) {
-            const mask = '+7 (___) ___ ____';
-
-
-            let i = 0,
-                value = target.value.replace(/\D/g, ""),
-                inputMask = mask.replace(/\D/g, "");
-
-            if (inputMask.lenght >= value.lenght) {
-                value = inputMask;
-            }
-
-            target.value = mask.replace(/./g, (a) => {
-                if (/[_\d]/.test(a) && i < value.length) {
-                    return value.charAt(i++);
-                } else if (i >= value.length) {
-                    return "";
-                } else {
-                    return a;
-                }
-            });
-
-        }
-    });
-
+    const countInputsPhones = document.querySelectorAll('.input-phone').length;
+    for (let i = 0; i < countInputsPhones; i++) {
+        maskPhone(`#feedback-input${i + 1}`);
+    }
 
     const checkInputs = (form) => {
         const allInputs = form.querySelectorAll('input');
